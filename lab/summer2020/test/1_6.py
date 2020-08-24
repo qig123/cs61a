f814979cd7475f1d2367abc83b67576feb76e7bf
guo_qiu <qiuguoxyz2@gmail.com>
2020-08-24 18:52:05 +0800

    cs61a_reading

--- lab/summer2020/test/1_6.py
+++ lab/summer2020/test/1_6.py
@@ -2,19 +2,32 @@ def square(x):
     return x*x
 
 
+def sum_naturals(n):
+    total, k = 0, 1
+    while k <= n:
+        total, k = total+k, k+1
+    return total
+
+
+def sum_cubes(n):
+    total, k = 0, 1
+    while k <= n:
+        total, k = total+k*k*k, k+1
+    return total
+
+
+def pi_sum(n):
+    total, k = 0, 1
+    while k <= n:
+        total, k = total + 8 / ((4*k-3) * (4*k-1)), k+1
+    return total
+
+
 def summation(n, term):
     total, k = 0, 1
     while k <= n:
         total, k = total+term(k), k+1
     return total
-
-
-def cube(x):
-    return x*x*x
-
-
-def sum_cubes(n):
-    return summation(n, cube)
 
 
 def improve(update, close, guess=1):
@@ -23,24 +36,16 @@ def improve(update, close, guess=1):
     return guess
 
 
-def golden_update(guess):
-    return 1/guess+1
-
-
-def square_close_to_successor(guess):
-    return approx_eq(guess*guess, guess+1)
+def average(x, y):
+    return (x+y)/2
 
 
 def approx_eq(x, y, tolerance=1e-15):
     return abs(x-y) < tolerance
 
 
-def average(x, y):
-    return (x+y)/2
-
-
 def sqrt(a):
-    def sqrt_update(x):
+    def sqrt_update(x, a):
         return average(x, a/x)
 
     def sqrt_close(x):
@@ -48,25 +53,7 @@ def sqrt(a):
     return improve(sqrt_update, sqrt_close)
 
 
-def curried_pow(x):
-    def h(y):
-        return pow(x, y)
-    return h
-
-
-def curried_pow_lambda(x):
-    return lambda y: pow(x, y)
-
-
-# print(curried_pow_lambda(2)(3))
-# print((lambda x: lambda y: pow(x, y))(2)(3))
-
-
 def compose1(f, g):
     def h(x):
         return f(g(x))
     return h
-
-
-f = compose1(lambda x: x+2, lambda x: x*x)
-print(f(5))
